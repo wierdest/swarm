37b0bc8a5aaff8c6679a39e40a40235f91bb9824
andre <andrlzpt@protonmail.com>
2025-08-17 11:38:58 -0300

    :broken_heart: Damage

---
+++ src/Swarm.Domain/Combat/Damage.cs
@@ -0,0 +1,14 @@
+﻿using Swarm.Domain.Common;
+
+namespace Swarm.Domain.Combat;
+
+public readonly record struct Damage
+{
+    public readonly int Value;
+
+    public Damage(int value)
+    {
+        Guard.NonNegative(value, nameof(Damage));
+        Value = value;
+    }
+}
