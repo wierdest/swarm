ed5f367c23d241e615d4c804359e8dc3a5a616cf
andre <andrlzpt@protonmail.com>
2025-08-17 12:04:02 -0300

    :alarm_clock: DeltaTime

---
+++ src/Swarm.Domain/Time/DeltaTime.cs
@@ -0,0 +1,16 @@
+﻿using Swarm.Domain.Common;
+
+namespace Swarm.Domain.Time;
+
+public readonly record struct DeltaTime
+{
+    public readonly float Seconds;
+
+    public DeltaTime(float seconds)
+    {
+        Guard.Positive(seconds, nameof(DeltaTime));
+        Seconds = seconds;
+    }
+
+    public static implicit operator float(DeltaTime deltaTime) => deltaTime.Seconds; 
+}
