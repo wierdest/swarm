0e6262760c93312412cd2c2e98acfa1d88d29edf
andre <andrlzpt@protonmail.com>
2025-08-17 18:05:20 -0300

    :video_game: Utilidade para integração de movimento

---
+++ src/Swarm.Domain/Physics/MovementIntegrator.cs
@@ -0,0 +1,17 @@
+﻿using Swarm.Domain.Primitives;
+using Swarm.Domain.Time;
+
+namespace Swarm.Domain.Physics;
+
+public static class MovementIntegrator
+{
+   public static Vector2 Advance(Vector2 pos, Direction dir, float speed, DeltaTime dt, Bounds stage)
+    {
+        var next = pos + dir.Vector * speed * dt;
+        return stage.Clamp(next);
+    }
+
+    public static Vector2 AdvanceUnclamped(Vector2 pos, Direction dir, float speed, DeltaTime dt)
+        => pos + dir.Vector * speed * dt;
+
+}
