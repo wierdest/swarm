55ddc82d4032d6d257c56d235ba8a942688cf2da
andre <andrlzpt@protonmail.com>
2025-08-17 11:09:10 -0300

    :left_right_arrow: Primitivo Direction

---
+++ src/Swarm.Domain/Primitives/Direction.cs
@@ -0,0 +1,18 @@
+﻿using Swarm.Domain.Common;
+
+namespace Swarm.Domain.Primitives;
+
+public readonly record struct Direction
+{
+    public readonly Vector2 Value;
+    public Direction(Vector2 value)
+    {
+        var lenSq = Value.LengthSquared();
+        Guard.True(lenSq > 0.999f && lenSq < 1.001f, "Direction must be normalized.");
+
+        Value = value;
+    }
+
+    public static Direction From(float x, float y) => new(new Vector2(x, y).Normalized());
+    public static Direction From(Vector2 vector2) => new(vector2.Normalized());
+}
