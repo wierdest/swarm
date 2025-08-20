4ecb373b526fc208b5d0c296e3864655a6f4ff78
andre <andrlzpt@protonmail.com>
2025-08-20 19:41:24 -0300

    :gun: WeaponConfig

---
+++ src/Swarm.Application/Contracts/WeaponConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Swarm.Application.Contracts;
+
+public readonly record struct WeaponConfig(
+    int Damage,
+    float ProjectileSpeed,
+    float ProjectileRadius,
+    float RatePerSecond,
+    float ProjectileLifetimeSeconds
+);
