8911fde43df65ffd3037e13849ec336b8e0b57cf
andre <andrlzpt@protonmail.com>
2025-08-20 19:57:43 -0300

    :camera: GameSnapshot

---
+++ src/Swarm.Application/Contracts/GameSnapshot.cs
@@ -0,0 +1,9 @@
+﻿using Swarm.Application.DTOs;
+
+namespace Swarm.Application.Contracts;
+
+public readonly record struct GameSnapshot(
+    BoundsDTO Stage,
+    PlayerDTO Player,
+    IReadOnlyList<ProjectileDTO> Projectiles
+);
