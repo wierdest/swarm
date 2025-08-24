5eb839a58df3f064f409b473251b392fa1e8a4d5
andre <andrlzpt@protonmail.com>
2025-08-24 15:23:44 -0300

    :space_invader: IEnemy herda ILivingEntity e implementa Tick conectado ao Player

---
+++ src/Swarm.Domain/Interfaces/IEnemy.cs
@@ -0,0 +1,9 @@
+﻿using Swarm.Domain.Primitives;
+using Swarm.Domain.Time;
+
+namespace Swarm.Domain.Interfaces;
+
+public interface IEnemy : ILivingEntity
+{
+    void Tick(DeltaTime dt, Vector2 playerPosition, Bounds stage);
+}
