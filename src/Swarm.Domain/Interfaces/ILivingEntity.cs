c597730d29b1c75cf2b467b73086b7d90d2f3f90
andre <andrlzpt@protonmail.com>
2025-08-24 15:15:14 -0300

    :sparkles: ILivingEntity inherits ICollidable

---
+++ src/Swarm.Domain/Interfaces/ILivingEntity.cs
@@ -0,0 +1,14 @@
+﻿using Swarm.Domain.Combat;
+using Swarm.Domain.Primitives;
+using Swarm.Domain.Time;
+
+namespace Swarm.Domain.Interfaces;
+
+public interface ILivingEntity : ICollidable
+{
+    EntityId Id { get; }
+    HitPoints HP { get; }
+    bool IsDead { get; }
+    void TakeDamage(Damage damage);
+
+}
