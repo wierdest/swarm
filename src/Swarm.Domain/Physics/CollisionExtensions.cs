8abca9552de60e2b5b5a17635882bcbe142ac224
andre <andrlzpt@protonmail.com>
2025-08-24 15:00:49 -0300

    :shield: ICollidable e CollisionExtensions para detectar colisões

---
+++ src/Swarm.Domain/Physics/CollisionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Swarm.Domain.Interfaces;
+
+namespace Swarm.Domain.Physics;
+
+public static class CollisionExtensions
+{
+    public static bool Intersects(this ICollidable a, ICollidable b)
+    {
+        var diff = a.Position - b.Position;
+        var radiusSum = a.Radius.Value + b.Radius.Value;
+        return diff.LengthSquared() <= radiusSum * radiusSum;
+    }
+}
