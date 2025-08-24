32eb1906d1c9f2395b2d361b29e7ebc0a5b729c7
andre <andrlzpt@protonmail.com>
2025-08-24 15:29:44 -0300

    :space_invader: BasicEnemy implementa IEnemy, recebe IEnemyBehaviour

---
+++ src/Swarm.Domain/Entities/BasicEnemy.cs
@@ -0,0 +1,40 @@
+﻿using Swarm.Domain.Combat;
+using Swarm.Domain.Interfaces;
+using Swarm.Domain.Physics;
+using Swarm.Domain.Primitives;
+using Swarm.Domain.Time;
+
+namespace Swarm.Domain.Entities;
+
+public sealed class BasicEnemy(
+    EntityId id,
+    Vector2 startPosition,
+    Radius radius,
+    HitPoints initialHitPoints,
+    IEnemyBehaviour behaviour
+) : IEnemy
+{
+    public EntityId Id { get; } = id;
+
+    public HitPoints HP { get; private set;  } = initialHitPoints;
+
+    public bool IsDead => HP.IsZero;
+
+    public Vector2 Position { get; private set; } = startPosition;
+
+    public Radius Radius { get; } = radius;
+
+    public bool CollidesWith(ICollidable other) => CollisionExtensions.Intersects(this, other);
+
+    public void TakeDamage(Damage damage)
+    {
+        HP = HP.Take(damage.Value);
+    }
+
+    public void Tick(DeltaTime dt, Vector2 playerPosition, Bounds stage)
+    {
+        var movement = behaviour.DecideMovement(Position, playerPosition, dt);
+        if (movement.HasValue)
+            Position = MovementIntegrator.Advance(Position, movement.Value.direction, movement.Value.speed, dt, stage);
+    }
+}
