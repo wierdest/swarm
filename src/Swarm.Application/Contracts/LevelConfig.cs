9e4cbc5a4274f2d5ba3a5d453d6d97a1dc4f7987
andre <andrlzpt@protonmail.com>
2025-08-31 17:55:08 -0300

    :trophy: LevelConfig

---
+++ src/Swarm.Application/Contracts/LevelConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Swarm.Application.Contracts;
+
+public readonly record struct LevelConfig(
+    WeaponConfig Weapon,
+    AreaConfig PlayerAreaConfig,
+    AreaConfig TargetAreaConfig,
+    IReadOnlyList<AreaConfig> Walls,
+    IReadOnlyList<SpawnerConfig> Spawners
+);
