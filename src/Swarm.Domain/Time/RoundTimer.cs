707befadb2da67568bb72a6e4a41e851be6b2450
andre <andrlzpt@protonmail.com>
2025-09-02 20:46:42 -0300

    :stopwatch: Timer para o Round do jogo!

---
+++ src/Swarm.Domain/Time/RoundTimer.cs
@@ -0,0 +1,34 @@
+﻿using Swarm.Domain.Common;
+
+namespace Swarm.Domain.Time;
+
+public readonly record struct RoundTimer
+{
+    public readonly int Seconds;
+
+    public RoundTimer(int seconds) : this()
+    {
+        Guard.NonNegative(seconds, nameof(seconds));
+        Guard.MaxSeconds(seconds, 999, nameof(seconds));
+        Seconds = seconds;
+    }
+
+    public bool IsExpired => Seconds <= 0;
+
+    public RoundTimer Reset(int seconds) => new(seconds);
+
+    public RoundTimer Tick(int delta = 1)
+    {
+        var newValue = Seconds - delta;
+        return new RoundTimer(Math.Max(newValue, 0));
+    }
+
+    public RoundTimer Add(int extraSeconds)
+    {
+        var newValue = Seconds + extraSeconds;
+        return new RoundTimer(Math.Min(newValue, 999));
+    }
+
+    public override string ToString() => $"{Seconds:000}s";
+    
+}
