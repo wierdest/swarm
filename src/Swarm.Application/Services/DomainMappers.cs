81fce106eb3705e7b070c1b4cb3c8f79e4f4dcd4
andre <andrlzpt@protonmail.com>
2025-09-03 20:24:12 -0300

    :earth_africa::stopwatch: DomainMapper mapeia Timer

--- src/Swarm.Application/Services/DomainMappers.cs
+++ src/Swarm.Application/Services/DomainMappers.cs
@@ -13,19 +13,19 @@ static class DomainMappers
         var stage = new BoundsDTO(s.Stage.Left, s.Stage.Top, s.Stage.Right, s.Stage.Bottom);
         var p = s.Player;
 
-        var playerRotation = MathF.Atan2(p.Rotation.Vector.Y, p.Rotation.Vector.X);
+        var playerRotation = MathF.Atan2(p.Rotation.Y, p.Rotation.X);
 
-        var player = new PlayerDTO(p.Position.X, p.Position.Y, p.Radius.Value, playerRotation);
+        var player = new PlayerDTO(p.Position.X, p.Position.Y, p.Radius, playerRotation);
 
         var projectiles = new List<ProjectileDTO>(s.Projectiles.Count);
         foreach (var proj in s.Projectiles)
-            projectiles.Add(new ProjectileDTO(proj.Position.X, proj.Position.Y, proj.Radius.Value));
+            projectiles.Add(new ProjectileDTO(proj.Position.X, proj.Position.Y, proj.Radius));
         
         var enemies = new List<EnemyDTO>(s.Enemies.Count);
         foreach (var e in s.Enemies)
         {
-            var enemyRotation = MathF.Atan2(e.Rotation.Vector.Y, e.Rotation.Vector.X);
-            enemies.Add(new EnemyDTO(e.Position.X, e.Position.Y, e.Radius.Value, enemyRotation));
+            var enemyRotation = MathF.Atan2(e.Rotation.Y, e.Rotation.X);
+            enemies.Add(new EnemyDTO(e.Position.X, e.Position.Y, e.Radius, enemyRotation));
         }
 
         // This is just a prototype
@@ -33,17 +33,19 @@ static class DomainMappers
         "A descrição do level muda quando chega na TargetArea" :
         "Esse é o level inicial de um protótipo.";
 
-        var hud = new HudDTO(s.Score.Value, p.HP.Value, enemies.Count, levelStateString);
+        levelStateString = s.IsTimeUp ? "TIME IS UP!!!" : levelStateString;
+
+        var hud = new HudDTO(s.Score.Value, p.HP.Value, s.TimeString, enemies.Count, levelStateString);
 
         var walls = new List<DrawableDTO>(s.Walls.Count);
         foreach (var w in s.Walls)
         {
-            walls.Add(new DrawableDTO(w.Position.X, w.Position.Y, w.Radius.Value));
+            walls.Add(new DrawableDTO(w.Position.X, w.Position.Y, w.Radius));
         }
 
-        var playerArea = new DrawableDTO(pA.Position.X, pA.Position.Y, pA.Radius.Value);
+        var playerArea = new DrawableDTO(pA.Position.X, pA.Position.Y, pA.Radius);
 
-        var targetArea = new DrawableDTO(t.Position.X, t.Position.Y, t.Radius.Value);
+        var targetArea = new DrawableDTO(t.Position.X, t.Position.Y, t.Radius);
 
         return new GameSnapshot(
             stage,
