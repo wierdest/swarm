aa6a009a0f2b34ca86d0301992ca3448b2965e1b
andre <andrlzpt@protonmail.com>
2025-09-09 20:02:02 -0300

    :ambulance: Fix namespace

--- src/Swarm.Domain/GameObjects/GameObject.cs
+++ src/Swarm.Domain/GameObjects/GameObject.cs
@@ -1,11 +1,12 @@
-﻿using Swarm.Domain.Interfaces;
+﻿using Swarm.Domain.Entities;
+using Swarm.Domain.Interfaces;
 using Swarm.Domain.Time;
 
-namespace Swarm.Domain.Entities;
+namespace Swarm.Domain.GameObjects;
 
 public abstract class GameObject(GameSession session) : IGameObject
 {
     protected readonly GameSession _session = session;
     public abstract void Tick(DeltaTime dt);
-
+        
 }
