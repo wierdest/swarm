202582d557db775c513fa1ed6c90c37f137e7fb2
andre <andrlzpt@protonmail.com>
2025-09-15 19:35:09 -0300

    :boom::gun: Projectiles têm owner e motion data criação de null weapon para inimigos que não tem arma

---
+++ src/Swarm.Domain/Entities/Projectiles/ProjectileMotionData.cs
@@ -0,0 +1,5 @@
+﻿using Swarm.Domain.Primitives;
+
+namespace Swarm.Domain.Entities.Projectiles;
+
+public readonly record struct ProjectileMotionData(Vector2 Position, Direction Direction, float Speed);
---
+++ src/Swarm.Domain/Entities/Weapons/Patterns/NoFirePattern.cs
@@ -0,0 +1,12 @@
+﻿using Swarm.Domain.Entities.Projectiles;
+using Swarm.Domain.Interfaces;
+using Swarm.Domain.Primitives;
+
+namespace Swarm.Domain.Entities.Weapons.Patterns;
+
+public sealed class NoFirePattern : IFirePattern
+{
+    public IEnumerable<Projectile> Fire(Vector2 origin, Direction facing, ProjectileOwnerTypes ownerType) => [];
+
+}
+
