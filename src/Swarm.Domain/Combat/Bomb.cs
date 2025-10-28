79a232d883acc13ab7a274f526864e8fc920c1d7
andre <andrlzpt@protonmail.com>
2025-10-28 20:29:15 -0300

    :bomb::sparkles: Bomb and bom factory... lost all my changes.... will have to wire everything up again

---
+++ src/Swarm.Domain/Combat/Bomb.cs
@@ -0,0 +1,14 @@
+﻿using Swarm.Domain.Time;
+
+namespace Swarm.Domain.Combat;
+
+public sealed class Bomb(
+    string identifier,
+    Cooldown cooldown
+)
+{
+    public string Identifier { get; } = identifier;
+    public Cooldown Cooldown { get; private set; } = cooldown;
+    public void Tick(DeltaTime dt) => Cooldown = Cooldown.Tick(dt);
+
+}
